031b3e0296b58e4614fe981c923fea2cc7f7e94c
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2019-05-05 21:55:33 -0400

    started writing the N-Arity versions of Choice and ChoiceT

--- src/Data/Choice.hs
+++ src/Data/Choice.hs
@@ -1,8 +1,13 @@
-{-# LANGUAGE FlexibleInstances, GADTs, RankNTypes, ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleInstances, GADTs, TypeOperators,PolyKinds, KindSignatures, DataKinds, RankNTypes, ScopedTypeVariables #-}
 module Data.Choice where
 
 import Data.Bifunctor
 import Data.Biapplicative
+
+data Pick :: [k] -> k -> * where
+  Absurd :: forall a .  Pick '[] a
+  Here :: forall a b . Pick (a ': b) a
+  Next ::  forall a bs c . Pick bs c -> Pick (a ':  bs) c
 
 data Choose a b c where
     CLeft  :: Choose a b a
@@ -11,3 +16,12 @@ data Choose a b c where
 newtype Choice a b = Choice (forall r . Choose  a b r -> r)
 newtype ChoiceT t a b = ChoiceT (forall r . Choose (t a) (t b) r -> r)
 
+data HSum :: [*] -> * where
+  HExists :: forall a as . Pick as a -> a -> HSum as
+
+data HSumT :: ( k -> * ) -> [k] -> *  where
+  HExistsT :: forall f a as . Pick as a -> f a -> HSumT f as
+
+newtype HChoice as = HChoice (forall r . Pick as r  -> r )
+
+newtype HChoiceT f as = HChoiceT (forall r . Pick as r -> f r )
